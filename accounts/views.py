ab105ff4a9e543875acf884c5bb0d523d88bfafa
Alces-Alces <40851599+Alces-Alces@users.noreply.github.com>
2021-01-13 11:01:30 +0100

    login

---
+++ accounts/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from .forms import RegisterForm
+
+@login_required
+def index(request):
+    return render(request,'accounts/index.html')
+def sign_up(request):
+    context = {}
+    form = UserCreationForm(request.POST or None)
+    if request.method == "POST":
+        if form.is_valid():
+            user = form.save()
+            login(request,user)
+            return render(request,'accounts/index.html')
+    context['form']=form
+    return render(request,'registration/sign_up.html',context)
+
+def register(response):
+    if response.method == "POST":
+        form = RegisterForm(response.POST)
+        if form.is_valid():
+            form.save()
+
+        return redirect("/")
+    else:
+        form = RegisterForm()
+
+    return render(response, "registration/register.html", {"form":form})
